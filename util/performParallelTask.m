5c22328a521c66096a15454acaf58f538bf9fa5f
Daniel Woschée <daniel.woschee@physik.lmu.de>
2019-01-30 18:28:53 +0100

    Own function for appending to worker matfile

--- util/performParallelTask.m
+++ util/performParallelTask.m
@@ -25,6 +25,14 @@ function [ Ffile ] = performParallelTask( Ffile, ntraces )
 % Suppress warning on parallel pool startup
 warning('off','MATLAB:datetime:NonstandardSystemTimeZone')
 
+% Get number of workers
+p = gcp();
+if isempty(p)
+	nWorkers = 0;
+else
+	nWorkers = p.NumWorkers;
+end
+
 parfor i = 1:ntraces
 %for i = 1:ntraces
 	% Read in temporary files
@@ -86,6 +94,26 @@ parfor i = 1:ntraces
 	% Evaluate fitted function
 	data_sim = model.simulate(mf.t_sim(1:mf.data_sim_len(file_ind,1),mf.t_sim_ind(file_ind,1)), params);
 
+	%% Postprocessing
+	% Predefine default result values
+	max_val = NaN;
+	max_ind = NaN;
+	min_val = NaN;
+	min_ind = NaN;
+	amplitude = NaN;
+	data_amp = NaN;
+	t_event = NaN;
+	event_deriv = NaN;
+	fit_parabola = [];
+	fit_type = NaN;
+	rising_edge_xoff = NaN;
+	rising_edge_yoff = NaN;
+	rising_edge_scale = NaN;
+	falling_edge_xoff = NaN;
+	falling_edge_yoff = NaN;
+	falling_edge_scale = NaN;
+
+	% Execute postprocessing function
 	if isfield(model, 'postproc') && isa(model.postproc, 'function_handle')
 		% Determine extrema and amplitude of fit
 		[max_val, max_ind] = max(data_sim);
@@ -108,182 +136,62 @@ parfor i = 1:ntraces
 		% Run model specific postprocessing routine
 		if isfield(model, 'postproc') && isa(model.postproc, 'function_handle')
 			[t_event, fit_info, event_deriv] = model.postproc(D, F, R);
-		else
-			t_event = NaN;
-			fit_info.type = NaN;
-			event_deriv = NaN;
 		end
 
 		fit_type = fit_info.type;
 		if isfield(fit_info, 'parabola')
 			fit_parabola = fit_info.parabola;
-		else
-			fit_parabola = [];
 		end
 
 		if isfield(fit_info, 'rising_edge_xoff')
 			rising_edge_xoff = fit_info.rising_edge_xoff;
-		else
-			rising_edge_xoff = NaN;
 		end
 		if isfield(fit_info, 'rising_edge_yoff')
 			rising_edge_yoff = fit_info.rising_edge_yoff;
-		else
-			rising_edge_yoff = NaN;
 		end
 		if isfield(fit_info, 'rising_edge_scale')
 			rising_edge_scale = fit_info.rising_edge_scale;
-		else
-			rising_edge_scale = NaN;
 		end
 
 		if isfield(fit_info, 'falling_edge_xoff')
 			falling_edge_xoff = fit_info.falling_edge_xoff;
-		else
-			falling_edge_xoff = NaN;
 		end
 		if isfield(fit_info, 'falling_edge_yoff')
 			falling_edge_yoff = fit_info.falling_edge_yoff;
-		else
-			falling_edge_yoff = NaN;
 		end
 		if isfield(fit_info, 'falling_edge_scale')
 			falling_edge_scale = fit_info.falling_edge_scale;
-		else
-			falling_edge_scale = NaN;
 		end
 
 	else
 		disp(['No postprocessing routine defined for model ' model.name]);
-		max_val = NaN;
-		max_ind = NaN;
-		min_val = NaN;
-		min_ind = NaN;
-		amplitude = NaN;
-		data_amp = NaN;
-		t_event = NaN;
-		event_deriv = NaN;
-		fit_parabola = [];
-		fit_type = NaN;
-		rising_edge_xoff = NaN;
-		rising_edge_yoff = NaN;
-		rising_edge_scale = NaN;
-		falling_edge_xoff = NaN;
-		falling_edge_yoff = NaN;
-		falling_edge_scale = NaN;
 	end
 
 	%% Write results to temporary matfile
-
-	% Open matfile related to this worker and save computation results
-	wfname = fullfile(mf.temp_dir, [ 'TEMP_' mf.time_now '_worker' num2str(w_id) '.mat' ]);
-
-	% Copy data to temporary matfile
-	if exist(wfname, 'file')
-		% Rename old temporary matfile
-		[path,name,ext] = fileparts(wfname);
-		wfoname = fullfile(path, [name '_old' ext]);
-		movefile(wfname, wfoname);
-
-		% Open old and new temporary matfile
-		wfo = matfile(wfoname);
-		wf = matfile(wfname);
-
-		% Write old and new data to new temporary matfile
-		wf.map(1:length(wfo.map)+1,1) = [wfo.map; i];
-
-		wf.max_val = [wfo.max_val; max_val];
-		wf.max_ind = [wfo.max_ind; max_ind];
-		wf.min_val = [wfo.min_val; min_val];
-		wf.min_ind = [wfo.min_ind; min_ind];
-		wf.amplitude = [wfo.amplitude; amplitude];
-		wf.data_amp = [wfo.data_amp; data_amp];
-		wf.logPost = [wfo.logPost; logPost];
-		wf.t_event = [wfo.t_event; t_event];
-		wf.event_deriv = [wfo.event_deriv; event_deriv];
-		wf.fit_type = [wfo.fit_type; fit_type];
-		wf.rising_edge_xoff = [wfo.rising_edge_xoff; rising_edge_xoff];
-		wf.rising_edge_yoff = [wfo.rising_edge_yoff; rising_edge_yoff];
-		wf.rising_edge_scale = [wfo.rising_edge_scale; rising_edge_scale];
-		wf.falling_edge_xoff = [wfo.falling_edge_xoff; falling_edge_xoff];
-		wf.falling_edge_yoff = [wfo.falling_edge_yoff; falling_edge_yoff];
-		wf.falling_edge_scale = [wfo.falling_edge_scale; falling_edge_scale];
-
-		d_len = length(data_sim) - size(wfo.data_sim, 1);
-		if d_len > 0
-			wf.data_sim = [ [wfo.data_sim; NaN(d_len, size(wfo.data_sim, 2))], ...
-				data_sim ];
-		elseif d_len < 0
-			wf.data_sim = [ wfo.data_sim, [data_sim; NaN(-d_len, 1)] ];
-		else
-			wf.data_sim = [ wfo.data_sim, data_sim ];
-		end
-
-		d_len = length(params) - size(wfo.params, 2);
-		if d_len > 0
-			wf.params = [ [wfo.params, NaN(size(wfo.params, 1), d_len)]; params ];
-		elseif d_len < 0
-			wf.params = [ wfo.params; [params, NaN(1, -d_len)] ];
-		else
-			wf.params = [ wfo.params; params ];
-		end
-
-		fit_parabola_new = wfo.fit_parabola;
-		fit_parabola_ind = uint32(0);
-		if ~isempty(fit_parabola)
-			fit_parabola_ind(1) = size(fit_parabola_new, 2) + 1;
-			fit_parabola_new( 1:length(fit_parabola), fit_parabola_ind ) = fit_parabola;
-		end
-		wf.fit_parabola = fit_parabola_new;
-		wf.fit_parabola_ind = [ wfo.fit_parabola_ind; fit_parabola_ind ];
-
-		% Delete old matfile
-		delete(wfoname);
-	else
-		% Create new matfile and write data to it
-		wf = matfile(wfname);
-		wf.map(1,1) = uint32(i);
-
-		wf.max_val(1,1) = single(max_val);
-		wf.max_ind(1,1) = uint32(max_ind);
-		wf.min_val(1,1) = single(min_val);
-		wf.min_ind(1,1) = uint32(min_ind);
-		wf.amplitude(1,1) = single(amplitude);
-		wf.data_amp(1,1) = single(data_amp);
-		wf.logPost(1,1) = single(logPost);
-		wf.t_event(1,1) = single(t_event);
-		wf.event_deriv(1,1) = single(event_deriv);
-		wf.fit_type(1,1) = int8(fit_type);
-		wf.rising_edge_xoff(1,1) = single(rising_edge_xoff);
-		wf.rising_edge_yoff(1,1) = single(rising_edge_yoff);
-		wf.rising_edge_scale(1,1) = single(rising_edge_scale);
-		wf.falling_edge_xoff(1,1) = single(falling_edge_xoff);
-		wf.falling_edge_yoff(1,1) = single(falling_edge_yoff);
-		wf.falling_edge_scale(1,1) = single(falling_edge_scale);
-
-		wf.data_sim(1:length(data_sim),1) = single(data_sim(:));
-		wf.params(1,1:length(params)) = single(params);
-
-		if isempty(fit_parabola)
-			wf.fit_parabola = single([]);
-			wf.fit_parabola_ind(1,1) = uint32(0);
-		else
-			wf.fit_parabola = single(fit_parabola);
-			wf.fit_parabola_ind(1,1) = uint32(1);
-		end
-	end
+	append_worker_file(mf.temp_dir, mf.time_now, w_id, i, ...
+		max_val, ...
+		max_ind, ...
+		min_val, ...
+		min_ind, ...
+		amplitude, ...
+		data_amp, ...
+		logPost, ...
+		t_event, ...
+		event_deriv, ...
+		fit_type, ...
+		rising_edge_xoff, ...
+		rising_edge_yoff, ...
+		rising_edge_scale, ...
+		falling_edge_xoff, ...
+		falling_edge_yoff, ...
+		falling_edge_scale, ...
+		data_sim, ...
+		params, ...
+		fit_parabola)
 end
 
 %% Perform cleanup (combine single worker matfiles to global matfile)
 disp([ get_time 'Cleaning up worker matfiles ...' ])
-
-% Get number of workers
-p = gcp('nocreate');
-if isempty(p)
-	nWorkers = 0;
-else
-	nWorkers = p.NumWorkers;
-end
 
 % Perform cleanup
 reduceData(Ffile, nWorkers);
